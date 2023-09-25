bd0a78ed22d2a6f7ce4adcc3bd03ca300a0e4307
MarcinSzymanek <48028985+MarcinSzymanek@users.noreply.github.com>
2023-09-25 20:38:33 +0200

    Added extensible UI resource panel prefab, UIResource and UIResourcePanel scripts, which update resource count. Added resource_update event to ResourceManager

---
+++ Assets/Scripts/AI/AIScan.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ObjectEnteredArgs : EventArgs
+{
+	public ObjectEnteredArgs(Transform t){
+		T = t;
+	}
+	public Transform T{get; set;}
+}
+
+public class AIScan : MonoBehaviour
+{
+	public delegate void ObjectEnteredHandler(object? sender, ObjectEnteredArgs args);
+	public event ObjectEnteredHandler objectEntered;
+    
+    
+	protected void OnTriggerEnter2D(Collider2D other)
+	{
+		objectEntered?.Invoke(this, new ObjectEnteredArgs(other.transform));
+	}
+}
---
+++ Assets/Scripts/UI/UIResource.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIResource : MonoBehaviour
+{
+	TextMeshProUGUI text_comp_;
+	public ResourceID ID;
+    void Awake()
+    {
+	    text_comp_ = GetComponentInChildren<TextMeshProUGUI>();
+    }
+    
+	public void UpdateText(string text){
+		text_comp_.text = text;
+	}
+
+}
