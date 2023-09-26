414de814cb335199d538a1a826e5fa80ac95dfd0
MarcinSzymanek <48028985+MarcinSzymanek@users.noreply.github.com>
2023-09-26 21:09:20 +0200

    Added new Title Scene! - Has a splash screen and a button panel - Play, Settings and Quit button - Quit now quits the game (or exits Editor PlayMode) - Added GameController class which keeps track of gamestate - Added UI/ImgRotateColours.cs which rotates image color on the script its attached to
    Moved SampleScene to Tests folder (which will hopefully untrack it)

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+	enum GameState{
+		title,
+		paused,
+		game,
+	}
+	
+	GameState state_;
+    
+	void Awake()
+	{
+		Scene s = SceneManager.GetActiveScene();
+		if(s.name == "TitleScene"){
+    		state_ = GameState.title;
+    	}
+	}
+    
+	public void StartNewGame(){
+		Debug.Log("Starting new session...");
+	}
+    
+	public void Quit(){
+		Debug.Log("Application exit.");
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+
+}
