628502c7e87fc7d13dc188fac761af9f97bc7dc6
au704650 <202106975@post.au.dk>
2023-09-29 14:18:20 +0200

    Now squares with the "empty" prefab can be created. Still need to get new prefabs for filling rooms and be able to connect rooms

--- Assets/Scripts/Generators/Dungeon Generator.cs
+++ Assets/Scripts/Generators/Dungeon Generator.cs
@@ -4,38 +4,94 @@ using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
-
     public GameObject wallTilePrefab = null;
-    // Start is called before the first frame update
+    public GameObject emptyTilePrefab = null;
+
+    struct Square
+    {
+        public Vector3 position;
+        public bool hasSingleEntrance;
+    }
+
+    List<Square> squares = new List<Square>();
+
     void Start()
     {
-        //Call the CreateSquare function
-        CreateSquare(10, 10);
+        // Call the function to generate squares
+        GenerateSquares(5);
     }
 
-
-    
-    
-    // Update is called once per frame
-    void Update()
+    void GenerateSquares(int amount)
     {
-        
-    }
-    
-    //Simple function for placing tiles
-    void PlaceTile(float pos_x, float pos_y, float pos_z) {
-        Vector3 spawnPosition = new Vector3(pos_x, pos_y, pos_z);
-        GameObject newWallTile = Instantiate(wallTilePrefab, spawnPosition, Quaternion.identity);
+        float size = 5f;
+        float gap = 1f; // Gap between squares
+
+        // Generate x-amount squares positioned randomly
+        for (int i = 0; i < amount; i++)
+        {
+            bool roomPlaced = false;
+            Vector3 position = Vector3.zero;
+
+            // Try placing the room until a non-overlapping position is found
+            while (!roomPlaced)
+            {
+
+                float x = Random.Range(-15f, 15f); // Random x position within a range
+                float y = Random.Range(-15f, 15f); // Random y position within a range
+                position = new Vector3(x, y, 0);
+
+                // Check for collisions with existing rooms
+                bool collisionDetected = false;
+                foreach (var square in squares)
+                {
+                    if (Mathf.Abs(position.x - square.position.x) < size + gap && Mathf.Abs(position.y - square.position.y) < size + gap)
+                    {
+                        collisionDetected = true;
+                        break;
+                    }
+                }
+
+                // If no collision, place the room
+                if (!collisionDetected)
+                {
+                    roomPlaced = true;
+                }
+            }
+
+            squares.Add(new Square { position = position });
+            CreateSquare(position);
+        }
     }
 
-    //Write a function that can generate a square/rectangle for now made of tiles
-    void CreateSquare(float x_length, float y_length) {
-        for (float i = 0; i <= x_length; i++) {
-            //Need to figure out a smart way that finds a proper position for a given dungeon room to be created
-            PlaceTile(5f + i, 5f, 5f);
-            PlaceTile(5f, 15f + i, 5f);
-            PlaceTile(15f, 5f + i, 5f);
-        } 
+    void CreateSquare(Vector3 position)
+    {
+        float size = 5f;
+        float halfSize = size / 2f;
+        float padding = 0.5f; // Padding to leave some space from the outer border
+
+        for (float i = -halfSize; i <= halfSize; i++)
+        {
+            for (float j = -halfSize; j <= halfSize; j++)
+            {
+                float posX = position.x + i;
+                float posY = position.y + j;
+                loat posZ = position.z;
+
+                // Check if the current position is on the outer border
+                bool isOuterBorder = Mathf.Abs(i) >= halfSize - padding || Mathf.Abs(j) >= halfSize - padding;
+
+                // Place wallTilePrefab on outer border, else place emptyTilePrefab
+                GameObject prefabToPlace = isOuterBorder ? wallTilePrefab : emptyTilePrefab;
+
+                PlaceTile(posX, posY, posZ, prefabToPlace);
+            }
+        }
     }
 
+
+    void PlaceTile(float posX, float posY, float posZ, GameObject prefab)
+    {
+        Vector3 spawnPosition = new Vector3(posX, posY, posZ);
+        Instantiate(prefab, spawnPosition, Quaternion.identity);
+    }   
 }
