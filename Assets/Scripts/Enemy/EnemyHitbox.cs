a8557dfd2bc858561fa5a2138f6184ff38448120
MarcinSzymanek <48028985+MarcinSzymanek@users.noreply.github.com>
2023-09-30 14:54:16 +0200

    Added DamageHitbox Prefab - It continually deals damage, but has a grace period - Changed EnemyHitbox script to facilitate that - We can reuse it for projectiles/other sources of contact damage - It is a prefab in Prefabs/Effects/
    Added DamageHitbox to the Slime
    Added Hitbox object to the Player
    - We should separate blocking hitboxes from taking damage hitboxes
    
    Changed Renderer2D to HOPEFULLY draw things correctly according to Y axis
    Added new PlayerHitbox layer. Separates hitbox that takes damage from other player components

--- Assets/Scripts/Enemy/EnemyHitbox.cs
+++ Assets/Scripts/Enemy/EnemyHitbox.cs
@@ -1,20 +1,29 @@
 ﻿using UnityEngine;
+using UnityEditor;
 
 public class EnemyHitbox : MonoBehaviour
 {
 
-    public int damageAmount;
-
-	private void OnTriggerEnter2D(Collider2D collision)
-	{
-		
-        Debug.Log("Colliding with player");
-        TakeDamageScript playerDamage = collision.gameObject.GetComponent<TakeDamageScript>();
-           
-        if (playerDamage != null)
-        {
-            playerDamage.TakeDamage(damageAmount);
-        }
+	public int damageAmount;
+	private int currentGrace = 0;
+	public int gracePeriod;
+	
+	void FixedUpdate(){
+		if(currentGrace > 0){
+			currentGrace--;
+		}
+	}
+    
+	private void OnTriggerStay2D(Collider2D collider){
+		if(currentGrace > 0) return;
+		currentGrace += gracePeriod;
+		TakeDamage tdamage = collider.transform.parent.GetComponent<TakeDamage>();
+		if (tdamage != null)
+		{
+			tdamage.TriggerTakeDamage(damageAmount);
+		}
         
-    }
+	}
 }
+
+
