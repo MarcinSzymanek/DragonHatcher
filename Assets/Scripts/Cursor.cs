2df67f941ed5ea67b2751b91ca658c618a7f262f
Alparslan Esen <37145259+Fragarach95@users.noreply.github.com>
2023-10-01 18:01:20 +0200

    the code, my bad the sickness fucked me over

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public Camera camera;
+public Transform obj;
+
+Vector2 objPos;
+Vector2 mousePos;
+float mousePosY, mousePosX;
+
 public class Cursor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  
 
     // Update is called once per frame
     void Update()
     {
-        
+    objPos = obj.transform.position;//gets player position
+	mousePos = Input.mousePosition;//gets mouse postion
+	mousePos = camera.ScreenToWorldPoint (mousePos);
+	mousePosX = mousePos.x - objPos.x;//gets the distance between object and mouse position for x
+	mousePosY = mousePos.y - objPos.y;//gets the distance between object and mouse position for y  if you want this.
     }
 }
