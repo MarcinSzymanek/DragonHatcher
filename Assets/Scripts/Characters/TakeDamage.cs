3deadba867a9222ca448fa3345117b087e219445
MarcinSzymanek <48028985+MarcinSzymanek@users.noreply.github.com>
2023-10-03 16:54:52 +0200

    Fixed accidental .gitignore change. Updated TakeDamage.cs

---
+++ Assets/Scripts/Characters/TakeDamage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDamage: MonoBehaviour
+{
+    // Events
+    public event System.Action<int> OnDamageTaken;
+    public event System.Action OnDeath;
+	
+	Health healthComponent;
+	
+	public void Awake(){
+		healthComponent = GetComponent<Health>();
+	}
+    // TakeDamage function to invoke damage event and possibly destroy object on death
+	public void TriggerTakeDamage(int amount)
+	{
+		Debug.Log("Trig take dmg");
+        OnDamageTaken?.Invoke(amount);
+        healthComponent.currentHealth -= amount;
+        if (healthComponent.currentHealth <= 0)
+        {
+            OnDeath?.Invoke();
+            Destroy(gameObject);
+        }
+    }
+}
+
+
+
