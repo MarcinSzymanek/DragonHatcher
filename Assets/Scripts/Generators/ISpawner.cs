0d88ebfd8d7c1e8d1e17872ab83c8dba68fb9855
Marcin Szymanek <nastrunach@gmail.com>
2023-10-15 23:27:53 +0200

    Added Spawner interface and ResourceSpawner script

---
+++ Assets/Scripts/Generators/ISpawner.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISpawner
+{
+	GameObject Spawn(Vector3 position);
+	GameObject Spawn(Vector3 position, Transform parent);
+}
---
+++ Assets/Scripts/Generators/ResourceSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceSpawner : MonoBehaviour, ISpawner
+{
+	public GameObject obj;
+    
+	public int countMax = 10;
+
+	private int totalResource = 0;
+
+	void Awake(){
+		totalResource = 0;
+	}
+
+	public GameObject Spawn(Vector3 position){
+		var newobj = Instantiate(obj, position, Quaternion.identity);
+		PickableItem item = newobj.GetComponent<PickableItem>();
+		item.Count = Random.RandomRange(0, countMax);
+		totalResource += item.Count;
+		return newobj;
+	}
+	
+	public GameObject Spawn(Vector3 position, Transform parent){
+		var newobj = Instantiate(obj, position, Quaternion.identity, parent);
+		PickableItem item = newobj.GetComponent<PickableItem>();
+		item.Count = Random.RandomRange(0, countMax);
+		totalResource += item.Count;
+		return newobj;
+	}
+}
