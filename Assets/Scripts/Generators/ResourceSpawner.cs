31a2fced1178492e7735a6368fb3b31759a65624
Marcin Szymanek <nastrunach@gmail.com>
2023-10-16 20:14:08 +0200

    Added button for spawning objects in the middle of the scene in Spawner script. Planning to make Spawner generic (since it can technically spawn anything) but not sure how yet.

--- Assets/Scripts/Generators/ResourceSpawner.cs
+++ Assets/Scripts/Generators/ResourceSpawner.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class ResourceSpawner : MonoBehaviour, ISpawner
+public class Spawner : MonoBehaviour
 {
-	public GameObject obj;
+	public GameObject[] objectPool;
     
 	public int countMax = 10;
 
@@ -14,19 +15,36 @@ public class ResourceSpawner : MonoBehaviour, ISpawner
 		totalResource = 0;
 	}
 
-	public GameObject Spawn(Vector3 position){
-		var newobj = Instantiate(obj, position, Quaternion.identity);
+	public GameObject Spawn(int index, Vector3 position){
+		var newobj = Instantiate(objectPool[index], position, Quaternion.identity);
 		PickableItem item = newobj.GetComponent<PickableItem>();
-		item.Count = Random.RandomRange(0, countMax);
+		item.Count = Random.Range(0, countMax);
 		totalResource += item.Count;
 		return newobj;
 	}
 	
-	public GameObject Spawn(Vector3 position, Transform parent){
-		var newobj = Instantiate(obj, position, Quaternion.identity, parent);
+	public GameObject Spawn(int index, Vector3 position, Transform parent){
+		var newobj = Instantiate(objectPool[index], position, Quaternion.identity, parent);
 		PickableItem item = newobj.GetComponent<PickableItem>();
-		item.Count = Random.RandomRange(0, countMax);
+		item.Count = Random.Range(0, countMax);
 		totalResource += item.Count;
 		return newobj;
 	}
+}
+
+// Spawn the last resource in the list at the center of scene
+[CustomEditor(typeof(Spawner))]
+public class CustomButton : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		Spawner myScript = (Spawner)target;
+		if (GUILayout.Button("Spawn"))
+		{
+			myScript.Spawn(myScript.objectPool.Length - 1, new Vector3(0, 0, 0));
+		}
+	}
+
 }
