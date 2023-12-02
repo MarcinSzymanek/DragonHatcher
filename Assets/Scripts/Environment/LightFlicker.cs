66e2fdd68c6af6d0693c85ce6c928dabc5bdae3f
Marcin Szymanek <nastrunach@gmail.com>
2023-12-02 03:27:50 +0100

    Added a new shadergraph and material - It adds an outline to the sprite

--- Assets/Scripts/Environment/LightFlicker.cs
+++ Assets/Scripts/Environment/LightFlicker.cs
@@ -9,30 +9,34 @@ public class LightFlicker : MonoBehaviour
     
 	private Color defaultColor;
 	private float defaultIntensity;
-	private float intensityStep = 0.1f;
+	private float intensityStep = 0.5f;
 	private float defaultOuterRadius;
-	private float outerRadiusStep = 0.2f;
+	private float outerRadiusStep = 0.4f;
 	private bool stepUp = false;
 	private int counter = 0;
     
 	void Awake()
     {
-	    light_ = transform.GetChild(0).GetComponent<UnityEngine.Rendering.Universal.Light2D>();
+	    light_ = transform.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
 	    defaultColor = light_.color;
 	    defaultIntensity = light_.intensity;
     }
     
+	void FixedUpdate(){
+		OnAnimationStep();
+	}
+    
 	private void OnAnimationStep(){
 		counter += 1;
 		
-		if(counter < 3) return;
+		if(counter < 30) return;
 		light_.intensity += (stepUp)? intensityStep: -intensityStep;
 		light_.pointLightOuterRadius += (stepUp)? outerRadiusStep: -outerRadiusStep;
 		if(stepUp) stepUp = false;
 		else stepUp = true;
-		if(counter > 3){
-			counter = 0;
-		}
+		
+		counter = 0;
+		
 		
 		
 	} 
