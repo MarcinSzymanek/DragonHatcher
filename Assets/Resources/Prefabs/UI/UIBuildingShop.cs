4e60212c3cfef207faef96d9d8ea8093125b3a84
Marcin Szymanek <nastrunach@gmail.com>
2023-12-10 01:14:19 +0100

    Added a UI frame for a building shop - New UI frame - Not much logic yet, gonna merge in the ballista first

---
+++ Assets/Resources/Prefabs/UI/UIBuildingShop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class UIBuildingShop : MonoBehaviour
+{
+	private int numberOfItems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	
+	public void AddItem(){
+		// Add a building item here
+	}
+}
