8a79c7cbdc71296b691b9a87ff71831cfe265720
Marcin Szymanek <nastrunach@gmail.com>
2023-12-13 23:27:50 +0100

    Implemented rudimentary scene transitions - Added a fadein and fadeout effect. Uses UI/BlackScreen object and FadeEffect script - EnemyGenerator now keeps track of enemies spawned and enemies left - EnemyGenerator will notify GameController when there are no more enemies left - Added SceneLoader script, responsible for switching between different scenes - New scene: Loading. Just a blank black screen with text in the middle - WaveDefense will switch to Loading when no more enemies left
    * Fixed slimes dealing damage to each other... why wasn't this bug in before?

--- Assets/Scripts/Managers/GameController.cs
+++ Assets/Scripts/Managers/GameController.cs
@@ -13,25 +13,28 @@ public class GameController : MonoBehaviour
 	
 	GameState state_;
 	InputManager input_;
+	SceneLoader sceneLoader_;
 	public SceneProperties.SceneType currentSceneType{get; private set;}
-    
+	
 	void Awake()
 	{
 		input_ = GameObject.FindObjectOfType<InputManager>();
-		
+		sceneLoader_ = FindObjectOfType<SceneLoader>();
 		Scene s = SceneManager.GetActiveScene();
+		// This is stupid, we should SceneProperties.SceneType instead.
 		if(s.name == "TitleScene"){
     		state_ = GameState.title;
 		}
 		SceneProperties sceneProperties = GameObject.FindObjectOfType<SceneProperties>();
 		currentSceneType = sceneProperties.sceneType;
+		if(currentSceneType == SceneProperties.SceneType.LOADING) return;
 		if(currentSceneType == SceneProperties.SceneType.WAVE_DEFENCE){
 			// Set egg dying as the lose condition
 			GameObject.Find("DragonEgg").GetComponent<DeathController>().objectDied += GameOver;
 		}
 		GameObject.Find("Player").GetComponent<DeathController>().objectDied += GameOver;
 	}
-    
+	    
 	public void StartNewGame(){
 		Debug.Log("Starting new session...");
 	}
@@ -49,7 +52,10 @@ public class GameController : MonoBehaviour
 		// Handle player losing the game: Either player died or the egg got destroyed
 		Debug.Log(args.ObjectName + " died! Game over...");
 		input_.DisableGameplayInput();
+		sceneLoader_.ChangeScene("Loading");
 	}
 	
-
+	public void OnWinCondition(){
+		sceneLoader_.ChangeScene("Loading");	
+	}
 }
---
+++ Assets/Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	FadeEffect fade_;
+	string nextScene = "Loading";
+	void Awake(){
+		fade_ =GameObject.Find("BlackScreen").GetComponent<FadeEffect>();
+		fade_.fadeOutFinished += OnFadeOutFinished;
+	}
+	
+	public void ChangeScene(string scenename){
+		nextScene = scenename;
+		fade_.ScreenFadeOut();
+	}
+	
+	void OnFadeOutFinished(){
+		SceneManager.LoadScene(nextScene);	
+	}
+}
