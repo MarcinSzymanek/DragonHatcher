c61999c58428d9e0b9c9d75c1d644d278711081b
Marcin Szymanek <nastrunach@gmail.com>
2023-12-15 03:29:49 +0100

    More fixes - Fixed an issue where player would be unable to cancel building in some cases - Fixed an issue (hopefully) where player wouldn't be able to build,    but the silhouette of the building would keep following the mouse cursor

--- Assets/Resources/Prefabs/UI/UIBuildingShop.cs
+++ Assets/Resources/Prefabs/UI/UIBuildingShop.cs
@@ -42,10 +42,8 @@ public class UIBuildingShop : MonoBehaviour
 			Debug.Log("Player tried to buy " + b.name + " but did not have enough resources");	
 			return;
 		}
-		Debug.Log(Input.mousePosition);
 		GameObject building = Instantiate(b.prefab, GameObject.FindObjectOfType<Camera>().ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
 		building.transform.SetParent(GameObject.Find("PlayerBuildings").transform);
-		Debug.Log("Updating cost in builder: " + b.cost.Count + b.cost[0].amount);
 		builderScript.UpdateSelected(building.GetComponent<Building>(), b.cost);
 		onBuildingCreated?.Invoke(this, new EventArgs());
 	}
