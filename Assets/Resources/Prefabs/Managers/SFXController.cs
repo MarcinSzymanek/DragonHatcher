756bd68286496669dd3bbbfc9688a57638616525
Marcin Szymanek <nastrunach@gmail.com>
2023-12-17 18:34:33 +0100

    Added new sfx for lightning_strike spell - Added animation event Lightning animation - Added public method to play sfx in SpellAnimEvents - Added a new class SFXController. It keeps track of played sfx and makes sure they're not played too loud when played together - Added a new class ManagedSFX. It makes the necessary calls to SFXController and does some cleanup - A new sfx sound is played when the lightning spell is used!

---
+++ Assets/Resources/Prefabs/Managers/SFXController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXController : MonoBehaviour
+{
+	List<AudioSource> managedAudioSources_;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+    	managedAudioSources_ = new	List<AudioSource>();    
+    }
+    
+	public void Detach(AudioSource source){
+		if(managedAudioSources_.Contains(source)) managedAudioSources_.Remove(source);
+	}
+    
+	public void PlaySound(AudioSource sourceToPlay, AudioClip clip){
+		if(!managedAudioSources_.Contains(sourceToPlay)) managedAudioSources_.Add(sourceToPlay);
+		float delay = 0f;
+		float volume = 1f;
+		foreach (var source in managedAudioSources_)
+		{
+			if(source.isPlaying) delay += 0.02f;
+			volume *= 0.5f; 
+		}
+		Debug.Log("Starting new sfx with volume: " + volume.ToString() + " , delay: " + delay.ToString());
+		StartCoroutine(delayedPlay(sourceToPlay, clip, delay, volume));
+		
+	}
+	
+	IEnumerator delayedPlay(AudioSource source, AudioClip clip, float delay, float volume){
+		yield return new WaitForSeconds(delay);
+		source.PlayOneShot(clip, volume);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Sound/ManagedSfx.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ManagedSFX : MonoBehaviour
+{
+	[field: SerializeField]
+	private AudioClip[] clips_;
+	private AudioSource source_;
+	private SFXController controller_;
+	
+	void Awake(){
+		controller_ = GameObject.FindFirstObjectByType<SFXController>();
+		source_ = GetComponent<AudioSource>();
+	}
+	
+	public void PlaySfx(){
+		if(clips_.Length != 0){
+			var clip = clips_[UnityEngine.Random.Range(0, clips_.Length)];
+			controller_.PlaySound(source_, clip);
+		}
+	}
+	
+	public void OnDetach(){
+		StartCoroutine(CleanUpSfx());
+	}
+	
+	IEnumerator CleanUpSfx(){
+		while(source_.isPlaying){
+			Debug.LogWarning("Sfx waiting for cleanup");
+			yield return null;
+		}
+		Destroy(gameObject);
+	}
+	
+	void OnDestroy(){
+		controller_.Detach(source_);
+	}
+}
