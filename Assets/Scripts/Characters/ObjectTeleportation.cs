825d279df1824a0a5b144c4fbd0f0378117d5e29
Marcin Szymanek <nastrunach@gmail.com>
2023-12-18 18:17:33 +0100

    Fixed unused import

--- Assets/Scripts/Characters/ObjectTeleportation.cs
+++ Assets/Scripts/Characters/ObjectTeleportation.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Searcher;
+// using UnityEditor.Searcher;
 using UnityEngine;
 
 public class ObjectTeleportation : MonoBehaviour
